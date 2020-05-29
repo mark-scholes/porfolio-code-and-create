37371e7c9b18cc8d43d40ae4204bcbe252fdbc8e
mark-scholes <innominatus15@outlook.com>
2020-05-29 03:25:36 +0100

    a few changes made to the look of the site. Everything is now working as it should be

--- script.js
+++ script.js
@@ -2,8 +2,14 @@ const navbar = document.querySelector(".navbar");
 const navbarOffsetTop = navbar.offsetTop;
 const sections = document.querySelectorAll("section");
 const links = document.querySelectorAll(".navbar-link");
+const progressBars = document.querySelector(".progress-bars-wrapper");
+const progressBarsPercent = [97, 89, 85, 87, 80];
 
 window.addEventListener("scroll", () => {
+  mainFn();
+});
+
+const mainFn = () => {
   window.pageYOffset >= navbarOffsetTop
     ? navbar.classList.add("sticky")
     : navbar.classList.remove("sticky");
@@ -17,4 +23,18 @@ window.addEventListener("scroll", () => {
       links[i].classList.add("change");
     }
   });
-});
+
+  if (window.pageYOffset + window.innerHeight >= progressBars.offsetTop) {
+    document.querySelectorAll(".progress-percent").forEach((percent, index) => {
+      percent.style.width = `${progressBarsPercent[index]}%`;
+      percent.previousElementSibling.firstElementChild.textContent =
+        progressBarsPercent[index];
+    });
+  }
+};
+
+mainFn();
+
+// window.addEventListener("resize", ()=> {
+//     window.location.reloa d()
+// })
